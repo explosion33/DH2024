2634c2196b2f191e026b85402a7581fc0f53c244
Nicholas Laustrup <nlaustrup91@gmail.com>
2024-10-12 16:31:44 -0700

    added login and logout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,27 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
-const config = getConfig();
-
-const providerConfig = {
-  domain: config.domain,
-  clientId: config.clientId,
-  onRedirectCallback,
-  authorizationParams: {
-    redirect_uri: window.location.origin,
-    ...(config.audience ? { audience: config.audience } : null),
-  },
-};
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <Auth0Provider
-        {...providerConfig}
-      >
-        <App />
+  <StrictMode>
+    <Auth0Provider
+      domain="dev-xzc0y1vwxvcjnrg1.us.auth0.com"
+      clientId="PmMWhDN8jvKXXoqLOxwCTEL1lPwlnYAc"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
 
-      </Auth0Provider>
-    </StrictMode>,
-  )
+    </Auth0Provider>
+  </StrictMode>,
+)
