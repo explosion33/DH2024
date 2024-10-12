bd03599de0d98243cf4cc9a9a4b84fe27e579086
Nicholas Laustrup <nlaustrup91@gmail.com>
2024-10-12 15:54:25 -0700

    adding auth0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  onRedirectCallback,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+    ...(config.audience ? { audience: config.audience } : null),
+  },
+};
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+      <Auth0Provider
+        {...providerConfig}
+      >
+        <App />
+
+      </Auth0Provider>
+    </StrictMode>,
+  )
