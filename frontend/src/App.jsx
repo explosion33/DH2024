ca858ab9d945e8578cd11cd978f20b80f5ce1a4c
vvill <contact@vvill.dev>
2024-10-12 17:40:07 -0700

    Fixed issues between oauth and shadcn implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import LoginButton from './components/LoginButton'
-import LogoutButton from './components/LogoutButton'
 import HomePage from './pages/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from "vite"
 
