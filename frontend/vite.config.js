4d2f76dac395f44bd65b47241928633e8f7d4a65
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2024-10-13 09:11:20 -0700

    wss fix attempt 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    hmr: {
+        host: "localhost",
+        protocol: "ws",
+    },
+  },
 })
