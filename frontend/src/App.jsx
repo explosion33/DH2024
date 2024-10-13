d1b1ea34a32d368774d33a3053876c7039cf7bd2
Nicholas Laustrup <nlaustrup91@gmail.com>
2024-10-12 17:40:18 -0700

    stashing for pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import HomePage from './pages/HomePage'
+import HomePage from './pages/HomePage';
 
 function App() {
 
